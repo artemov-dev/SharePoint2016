7bb39bc4ea1e05c65ab5f3a741ba921e451683cb
Александр Артемов <aleksandr.artemov@outlook.com>
2020-12-21 10:38:05 +0300

    src

---
+++ CSRReactForms/Layouts/CSRReactForms/src/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {PivotLargeExample} from './pivot.tsx'
+
+ReactDOM.render(
+    <PivotLargeExample />,
+    document.getElementById('MainPage')
+)
