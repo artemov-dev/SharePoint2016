cc00745afa84ecb49cb02a9cca6691315d7d6d73
Александр Артемов <aleksandr.artemov@outlook.com>
2021-02-18 13:49:16 +0300

    BDC

---
+++ BDCAdventure/BdcAdventure/AdventureService.cs
@@ -0,0 +1,26 @@
+﻿using BDCAdventure;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace BDCAdventure.BdcAdventure
+{
+    public partial class AdventureService
+    {
+        private static string connStr = "Data Source=SP2016;Initial Catalog=AdventureWorks2016;uid=advuser;pwd=123qweAa";
+        public static vEmployee ReadItem(int businessEntityID)
+        {
+            AdventureDBDataContext dataContext = new AdventureDBDataContext(connStr);
+            return dataContext.vEmployees.Where(e => e.BusinessEntityID.Equals(businessEntityID)).Single();
+        }
+
+
+
+        public static IEnumerable<vEmployee> ReadList()
+        {
+            AdventureDBDataContext dataContext = new AdventureDBDataContext(connStr);
+            return dataContext.vEmployees.ToList();
+        }
+    }
+}
---
+++ BDCAdventure/Features/Adventure/Adventure.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="481ba8db-07c8-444b-8d4b-0fc79bdc1744" description="This Feature contain BDC Connector" featureId="481ba8db-07c8-444b-8d4b-0fc79bdc1744" imageUrl="" scope="Farm" solutionId="00000000-0000-0000-0000-000000000000" title="BDCAdventure" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="09d2c2f2-162f-4b7d-b20a-c7c050ece6b2" />
+  </projectItems>
+</feature>
