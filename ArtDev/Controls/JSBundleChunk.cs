6b407fca4278f01a8b6333d5046c73ab842954b7
Александр Артемов <aleksandr.artemov@outlook.com>
2021-02-18 13:49:39 +0300

    JSBundle Hash

--- ArtDev/Controls/JSBundleChunk.cs
+++ ArtDev/Controls/JSBundleChunk.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 
@@ -7,18 +11,35 @@ namespace ArtDev.Controls
     public class JSBundleChunk : UserControl
     {
         public string JSBundleFile { get; set; }
+        private string GetMD5Hash(string filename)
+        {
+            string hive = SPUtility.GetGenericSetupPath(string.Empty) + "TEMPLATE\\"; 
+            using (var md5 = MD5.Create())
+            {
+                filename = filename.Replace("/15/", "/").Replace("/14/", "/").Replace("/12/", "/").Replace("/_layouts/", "/layouts/");
+                try
+                {
+                    using (var stream = File.OpenRead(hive + filename))
+                    {
+                        var hash = md5.ComputeHash(stream);
+                        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                    }
+                }
+                catch (Exception e) { return ""; }
+            }
+        }
 
-       
         protected override void CreateChildControls()
         {          
             string mode = (string)SPContext.Current.Web.AllProperties["DEV"] ?? null;
             if (mode != "true")
             {
                 string file = this.JSBundleFile;
+                string hash = GetMD5Hash(this.JSBundleFile);
 
                 HtmlGenericControl script = new HtmlGenericControl("script");
                 script.Attributes.Add("type", "text/javascript");
-                script.Attributes.Add("src", file);
+                script.Attributes.Add("src", file + "?hash=" + hash);
                 this.Controls.AddAt(0, script);
             }
         }
--- ArtDev/Controls/JSBundleStartUp.cs
+++ ArtDev/Controls/JSBundleStartUp.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
@@ -9,11 +13,31 @@ namespace ArtDev.Controls
     {
         public string JSBundleFile { get; set; }
 
+        private string GetMD5Hash(string filename)
+        {
+            string hive = SPUtility.GetGenericSetupPath(string.Empty) + "TEMPLATE\\";
+            using (var md5 = MD5.Create())
+            {
+                filename = filename.Replace("/15/", "/").Replace("/14/", "/").Replace("/12/", "/").Replace("/_layouts/", "/layouts/");
+                try
+                {
+                    using (var stream = File.OpenRead(hive + filename))
+                    {
+                        var hash = md5.ComputeHash(stream);
+                        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                    }
+                }
+                catch (Exception e) { return ""; }
+            }
+        }
+
+
         protected override void CreateChildControls()
         {
+            string hash = GetMD5Hash(this.JSBundleFile);
             string mode = (string)SPContext.Current.Web.AllProperties["DEV"] ?? null;
             string filename = this.JSBundleFile.Substring(this.JSBundleFile.LastIndexOf('/') + 1);
-            string file = mode == "true" ? "http://localhost:3000/public/" + filename : this.JSBundleFile;
+            string file = mode == "true" ? "http://localhost:3000/public/" + filename : this.JSBundleFile + "?hash=" + hash;
 
             ClientScriptManager csm = this.Page.ClientScript;
             //Render the function invocation. 
